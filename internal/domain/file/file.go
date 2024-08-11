fad2156a0a2e3c1f3ebae2e04c731024505b8b84
hungbui <hungbui2108@gmail.com>
2024-08-11 11:30:18 +0700

    first commit

---
+++ internal/domain/file/file.go
@@ -0,0 +1,13 @@
+package filemanager
+
+type File struct {
+	Id             string `bson:"_id,omitempty" json:"id"`
+	Name           string `bson:"name" json:"name"`
+	IsFolder       bool   `bson:"is_folder" json:"is_folder"`
+	Size           int    `bson:"size" json:"size"`
+	Path           string `bson:"path" json:"path"`
+	ParentFolder   string `bson:"parent_folder" json:"parent_folder"`
+	AncestorFolder string `bson:"ancestor_folder" json:"ancestor_folder"`
+	CreatedDate    string `bson:"created_date" json:"created_date"`
+	UpdatedDate    string `bson:"updated_date" json:"updated_date"`
+}
